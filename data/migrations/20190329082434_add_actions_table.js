2d01893666476ad7cf3e3a1d8eb1687c0b617f61
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-03-29 08:30:53 +0000

    MVP: Add actions migration

---
+++ data/migrations/20190329082434_add_actions_table.js
@@ -0,0 +1,26 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('actions', function(tbl) {
+    tbl.increments();
+    tbl
+        .string('description', 255)
+        .notNullable();
+    tbl
+        .string('notes', 255)
+        .notNullable();
+    tbl
+        .boolean('completed')
+        .defaultTo(false);
+    tbl
+        .integer('project_id')
+        .unsigned()
+        .references('id')
+        .inTable('projects')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('actions');
+};
